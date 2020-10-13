081f8c2a15b0dd6983d935db6266e3e77fe9e150
Anh Trinh <anh.trinhtrung@gmail.com>
2020-10-14 11:44:49 +1300

    CH10: function lifetime annotation

--- ch10-generic-types-traits/lifetime.rs
+++ ch10-generic-types-traits/lifetime.rs
@@ -1,10 +1,17 @@
-fn main() {
-    let r;
-
-    {
-        let x = 5;
-        r = &x;
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
 
-    println!("r: {}", r);
+}
+
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+
+    println!("The longest string is {}", result);
 }

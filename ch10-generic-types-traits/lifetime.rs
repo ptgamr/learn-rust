7b08e0fe7b6aea3151023fd54c129da223055385
Anh Trinh <anh.trinhtrung@gmail.com>
2020-10-14 12:44:26 +1300

    Lifetime Annotations in Struct Definitions

--- ch10-generic-types-traits/lifetime.rs
+++ ch10-generic-types-traits/lifetime.rs
@@ -4,7 +4,10 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     } else {
         y
     }
+}
 
+struct ImportantExcerpt<'a> {
+    part: &'a str,
 }
 
 fn main() {
@@ -14,4 +17,17 @@ fn main() {
     let result = longest(string1.as_str(), string2);
 
     println!("The longest string is {}", result);
+
+    // ======================
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let parts = novel.split('.').next();
+    println!("splitted {:#?}", parts);
+
+
+    let first_sentence = novel.split('.').next().unwrap();
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+    println!("ImportantExcerpt {}", i.part);
 }

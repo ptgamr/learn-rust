09754b57c51d269798ddbf49120eb1828b5a4b12
Anh Trinh <anh.trinhtrung@gmail.com>
2020-10-14 14:40:59 +1300

    Checking for Panics with should_panic

--- ch11-tests/adder/src/lib.rs
+++ ch11-tests/adder/src/lib.rs
@@ -1,5 +1,20 @@
 pub fn greeting(name: &str) -> String {
-    format!("Hello!")
+    format!("Hello {}!", name)
+}
+
+pub struct Guess {
+    #[allow(dead_code)]
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess must be between 1 - 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
 }
 
 
@@ -20,5 +35,12 @@ mod tests {
             "Greeting did not contain name, value was {}",
             result,
         );
+    }
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        let guess = Guess::new(200);
+        assert!(guess.value)
     }
 }

fb43f22d6ac13a9f698c8795dc1527edf94d391a
Anh Trinh <anh.trinhtrung@gmail.com>
2020-10-14 13:16:36 +1300

    Generic Type Parameters, Trait Bounds, and Lifetimes together

--- ch10-generic-types-traits/lifetime.rs
+++ ch10-generic-types-traits/lifetime.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
@@ -10,25 +12,38 @@ struct ImportantExcerpt<'a> {
     part: &'a str,
 }
 
+fn longest_with_an_announcement<'a, T> (
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str 
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn main() {
     let string1 = String::from("abcd");
     let string2 = "xyz";
 
     let result = longest(string1.as_str(), string2);
-
     println!("The longest string is {}", result);
+
+    let result2 = longest_with_an_announcement("xxx", "xxxxx", "Great job!");
+    println!("The longest string is {}", result2);
 
     // ======================
 
-    let first_sentence;
-
-    {
-        let novel;
-        novel = String::from("Call me Ishmael. Some years ago...");
-        let parts = novel.split('.').next();
-        println!("splitted {:#?}", parts);
-        first_sentence = novel.split('.').next().unwrap().clone();
-    }
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let parts = novel.split('.').next();
+    println!("splitted {:#?}", parts);
+    let first_sentence = novel.split('.').next().unwrap();
 
     let i = ImportantExcerpt {
         part: first_sentence,

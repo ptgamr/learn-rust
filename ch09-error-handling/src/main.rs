b2db48adddcca23f0ed1d2ed6fc9cf8337dfada0
Anh Trinh <anh.trinhtrung@gmail.com>
2020-10-14 16:19:41 +1300

    Question mark operator

--- ch09-error-handling/src/main.rs
+++ ch09-error-handling/src/main.rs
@@ -1,10 +1,39 @@
 use std::fs::File;
+use std::io;
 use std::io::ErrorKind;
+use std::io::Read;
+
+// without question mark operator
+// has to do lots of matching
+#[allow(dead_code)]
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("Hello.txt");
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+// more concise code with question mark operator
+#[allow(dead_code)]
+fn read_username_from_file_question_mark_operator() -> Result<String, io::Error> {
+    let mut f = File::open("Hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
 
 fn main() {
     let f = File::open("hello.txt");
 
-    let f = match f {
+    match f {
         Ok(file) => file,
         Err(error) => match error.kind() {
             ErrorKind::NotFound => match File::create("hello.txt") {

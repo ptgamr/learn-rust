3d94b9fe86042475e1708abf41f5b8167aa2685d
Anh Trinh <anh.trinhtrung@gmail.com>
2020-10-29 17:10:33 +1300

    Iterator:filter: use closures to capture their environment

--- ch13-closures/iterator/src/main.rs
+++ ch13-closures/iterator/src/main.rs
@@ -8,50 +8,53 @@ fn main() {
     }
 }
 
-#[test]
-fn iterator_demonstration() {
-    let v1 = vec![1, 2, 3];
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn iterator_demonstration() {
+        let v1 = vec![1, 2, 3];
 
-    let mut v1_iter = v1.iter();
+        let mut v1_iter = v1.iter();
 
-    assert_eq!(v1_iter.next(), Some(&1));
-}
-
-#[test]
-fn iterator_sum() {
-    let v1 = vec![1, 2, 3];
-
-    let v1_iter = v1.iter();
-
-    // need to give a type here because rust can't figure that out
-    let total: i32 = v1_iter.sum();
-
-    assert_eq!(total, 6);
-
-    // can not use v1_iter here
-    // because `sum` takes ownership of the iterator
-    // --> We call it "Consume the Iterator"
-
-}
-
-#[test]
-fn iterator_map_closure() {
-    let v1 = vec![1, 2, 3];
-
-    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
-
-    assert_eq!(v2, vec![2,3,4]);
-}
-
-#[test]
-fn iterator_map_fn() {
-    let v1 = vec![1, 2, 3];
-
-    fn plus_one(x: &i32) -> i32 {
-        x + 1
+        assert_eq!(v1_iter.next(), Some(&1));
     }
 
-    let v2: Vec<_> = v1.iter().map(plus_one).collect();
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
 
-    assert_eq!(v2, vec![2,3,4]);
+        let v1_iter = v1.iter();
+
+        // need to give a type here because rust can't figure that out
+        let total: i32 = v1_iter.sum();
+
+        assert_eq!(total, 6);
+
+        // can not use v1_iter here
+        // because `sum` takes ownership of the iterator
+        // --> We call it "Consume the Iterator"
+
+    }
+
+    #[test]
+    fn iterator_map_closure() {
+        let v1 = vec![1, 2, 3];
+
+        let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+        assert_eq!(v2, vec![2,3,4]);
+    }
+
+    #[test]
+    fn iterator_map_fn() {
+        let v1 = vec![1, 2, 3];
+
+        fn plus_one(x: &i32) -> i32 {
+            x + 1
+        }
+
+        let v2: Vec<_> = v1.iter().map(plus_one).collect();
+
+        assert_eq!(v2, vec![2,3,4]);
+    }
 }

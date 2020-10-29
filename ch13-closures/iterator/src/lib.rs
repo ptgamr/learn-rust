a053cfd3281ba2bbe9c3594570ce2c841845fa8e
Anh <anh.trinhtrung@gmail.com>
2020-10-30 02:17:55 +1300

    Custom iterator

--- ch13-closures/iterator/src/lib.rs
+++ ch13-closures/iterator/src/lib.rs
@@ -16,6 +16,34 @@ fn shoes_in_my_size(shoes: &Vec<Shoe>, shoe_size: u32) -> Vec<&Shoe> {
     shoes.into_iter().filter(|s| s.size == shoe_size).collect()
 }
 
+// ===================
+// CUSTOM ITERATOR
+// A simpler that returning the values from 1 --> 5
+// ===================
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    #[allow(dead_code)]
+    fn new() -> Counter {
+        Counter {count: 0}
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -49,5 +77,34 @@ mod tests {
                 &shoes[2],
             ]
         );
+    }
+
+    #[test]
+    fn calling_custom_iterator() {
+        let mut counter = Counter::new();
+
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+    }
+
+    #[test]
+    fn using_other_iterator_trait_method() {
+        // all of these method calls are possible because we specified how the `next` method
+        // works, and the standard library provides default implementations for other method
+        // that call `next`
+        let sum: u32 = Counter::new()
+            // zip produces only for pair
+            // because the theoretical fifth pair (5, None) is never produced
+            // zip returns None when either of its input iterators return None
+            .zip(Counter::new().skip(1))
+            .map(|(a, b)| a * b)
+            .filter(|x| x % 3 == 0)
+            .sum();
+
+        assert_eq!(18, sum);
     }
 }

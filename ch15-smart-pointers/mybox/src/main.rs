46f67b69f00615e317f470d074292db8a68d4ad3
Anh Trinh <anh.trinhtrung@gmail.com>
2020-10-30 16:13:46 +1300

    CH15: implement Deref trait to make it work

--- ch15-smart-pointers/mybox/src/main.rs
+++ ch15-smart-pointers/mybox/src/main.rs
@@ -1,8 +1,21 @@
+use std::ops::Deref;
+
 struct MyBox<T>(T);
 
 impl<T> MyBox<T> {
     fn new(x: T) -> MyBox<T> {
         MyBox(x)
+    }
+}
+
+// The Deref trait, provided by standard library
+// requires to implement one method named `deref` that borrow `self` and
+// returns a reference to the inner data.
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
     }
 }
 
